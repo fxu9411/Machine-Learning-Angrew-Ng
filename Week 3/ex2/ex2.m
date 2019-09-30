6093b9f1cd8010105ead0a4f47de563eff4ea036
Frank Xu <franklolxu@gmail.com>
2019-09-30 20:48:18 +0800

    Week 3 Vectorized
    100/100

--- Week 3/ex2/ex2.m
+++ Week 3/ex2/ex2.m
@@ -83,7 +83,7 @@ fprintf(' %f \n', grad);
 fprintf('Expected gradients (approx):\n 0.043\n 2.566\n 2.647\n');
 
 fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+pause();
 
 
 %% ============= Part 3: Optimizing using fminunc  =============
