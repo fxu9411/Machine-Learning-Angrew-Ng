7d05b93981f6f35e6845bb81f3b5cc21b6112e70
Frank Xu <franklolxu@gmail.com>
2019-10-07 19:24:58 +0800

    Finish week 6

--- Week 6/ex5/learningCurve.m
+++ Week 6/ex5/learningCurve.m
@@ -53,6 +53,19 @@ error_val   = zeros(m, 1);
 
 % ---------------------- Sample Solution ----------------------
 
+for i = 1:m
+  % Compute train/cross validation errors using training examples
+  % X(1:i, :) and y(1:i), storing the result in 
+  % error_train(i) and error_val(i)
+  Xtrain = X(1:i, :);
+  Ytrain = y(1:i);
+  
+  theta = trainLinearReg(Xtrain, Ytrain, lambda);
+  
+  error_train(i) = linearRegCostFunction(Xtrain, Ytrain, theta, 0);
+  error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
+  
+endfor
 
 
 
